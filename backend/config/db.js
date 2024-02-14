ea85541e3b5529a069fcf00827992797096bf241
unknown <isruabebaw@gmail.com>
2024-02-14 14:39:40 +0300

    the latest vertion final

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
    
     try {
         // console.log(process.env.MONGO_DB)
-        const conn = await mongoose.connect(process.env.MONGO_DB)
+        const conn = await mongoose.connect(process.env.MONGO_DB1)
 
         console.log(`mongoDB connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
