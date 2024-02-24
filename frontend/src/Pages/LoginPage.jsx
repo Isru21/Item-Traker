9a31f6d10855f4f168a95d22973b5d2f7516f7db
unknown <isruabebaw@gmail.com>
2024-02-24 15:55:25 +0300

    login changed to work with live backend

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
    
 
     try {
-      const response = await fetch("http://localhost:5000/api/users/login", {
+      const response = await fetch("https://item-traker.onrender.com/api/users/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
